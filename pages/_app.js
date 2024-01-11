56d5b53fbbd1b01a776584a36286087443a67e1e
Dev.Ezra <92317816+HackEzra@users.noreply.github.com>
2024-01-11 12:52:47 +0300

    page files:)

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../node_modules/react-modal-video/scss/modal-video.scss';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
